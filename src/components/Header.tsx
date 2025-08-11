435409112ce1bd18b03675582d69584737bd5edd
amsprimedigitalcore-hue <amsprimedigitalcore@gmail.com>
2025-08-10 23:01:51 -0700

    Fix Reviews button to link to What Our Clients Say section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -189,10 +189,23 @@ const Header: React.FC = () => {
                 </button>
                 <button
                   onClick={() => {
-                    scrollToSection('reviews');
+                    if (location.pathname !== '/') {
+                      navigate('/');
+                      setTimeout(() => {
+                        const element = document.getElementById('reviews');
+                        if (element) {
+                          element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                        }
+                      }, 100);
+                    } else {
+                      const element = document.getElementById('reviews');
+                      if (element) {
+                        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                      }
+                    }
                     setIsMobileMenuOpen(false);
                   }}
-                  className="block font-anton text-lg text-[#2a3747] hover:text-[#ffbe4a] hover:bg-gray-50 transition-all duration-300 px-6 py-3 rounded-lg mx-4 text-left w-[calc(100%-2rem)]"
+                  className="block font-anton text-lg text-[#2a3747] hover:text-[#ffbe4a] hover:bg-gray-50 transition-all duration-300 px-6 py-3 rounded-lg mx-4 text-left w-full"
                 >
                   Reviews
                 </button>
